cf2a649a43553abf0bc42a455bd2b4f1c069743b
Kartik-Vyas <101496656+Kartik-Vyas@users.noreply.github.com>
2022-05-13 20:28:44 +0530

    Create MarioGame.java

---
+++ UsingInterface/MarioGame.java
@@ -0,0 +1,20 @@
+package com.vyas.learnspringframework.game;
+
+public class MarioGame implements GameConsole{
+        public void up()
+        {
+            System.out.println("jump");
+        }
+        public void down()
+        {
+            System.out.println("go into tunnel");
+        }
+        public void left()
+        {
+            System.out.println("stop");
+        }
+        public void right()
+        {
+            System.out.println("forward");
+        }
+}
